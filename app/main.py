e0082b0aa2f88b526bba1dec967947344ef20fb4
Vighnesh Devkate <138555226+vighnesh-devkate@users.noreply.github.com>
2024-07-06 16:00:10 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,30 +6,24 @@ import numpy as np
 import tensorflow as tf
 import streamlit as st
 
-
+# Define the working directory and model path
 working_dir = os.path.dirname(os.path.abspath(__file__))
 model_path = f"{working_dir}/trained_model/plant_disease_prediction_model.h5"
+
 # Load the pre-trained model
 model = tf.keras.models.load_model(model_path)
 
-# loading the class names
+# Load the class names
 class_indices = json.load(open(f"{working_dir}/class_indices.json"))
-
 
 # Function to Load and Preprocess the Image using Pillow
 def load_and_preprocess_image(image_path, target_size=(224, 224)):
-    # Load the image
     img = Image.open(image_path)
-    # Resize the image
     img = img.resize(target_size)
-    # Convert the image to a numpy array
     img_array = np.array(img)
-    # Add batch dimension
     img_array = np.expand_dims(img_array, axis=0)
-    # Scale the image values to [0, 1]
     img_array = img_array.astype('float32') / 255.
     return img_array
-
 
 # Function to Predict the Class of an Image
 def predict_image_class(model, image_path, class_indices):
@@ -38,7 +32,6 @@ def predict_image_class(model, image_path, class_indices):
     predicted_class_index = np.argmax(predictions, axis=1)[0]
     predicted_class_name = class_indices[str(predicted_class_index)]
     return predicted_class_name
-
 
 # Streamlit App
 st.title('Plant Disease Classifier')
@@ -55,6 +48,52 @@ if uploaded_image is not None:
 
     with col2:
         if st.button('Classify'):
-            # Preprocess the uploaded image and predict the class
             prediction = predict_image_class(model, uploaded_image, class_indices)
             st.success(f'Prediction: {str(prediction)}')
+
+# Sidebar
+st.sidebar.title("Dashboard")
+app_mode = st.sidebar.selectbox("Select Page", ["Home", "About", "Disease Recognition"])
+
+# Main Page
+if app_mode == "Home":
+    st.header("PLANT DISEASE RECOGNITION SYSTEM")
+    image_path = "home_page.jpeg"
+    st.image(image_path, use_column_width=True)
+    st.markdown("""
+    Welcome to the Plant Disease Recognition System! 🌿🔍
+    
+    Our mission is to help in identifying plant diseases efficiently. Upload an image of a plant, and our system will analyze it to detect any signs of diseases. Together, let's protect our crops and ensure a healthier harvest!
+
+    ### How It Works
+    1. **Upload Image:** Go to the **Disease Recognition** page and upload an image of a plant with suspected diseases.
+    2. **Analysis:** Our system will process the image using advanced algorithms to identify potential diseases.
+    3. **Results:** View the results and recommendations for further action.
+
+    ### Why Choose Us?
+    - **Accuracy:** Our system utilizes state-of-the-art machine learning techniques for accurate disease detection.
+    - **User-Friendly:** Simple and intuitive interface for seamless user experience.
+    - **Fast and Efficient:** Receive results in seconds, allowing for quick decision-making.
+
+    ### Get Started
+    Click on the **Disease Recognition** page in the sidebar to upload an image and experience the power of our Plant Disease Recognition System!
+
+    ### About Us
+    Learn more about the project, our team, and our goals on the **About** page.
+    """)
+
+# About Project
+elif app_mode == "About":
+    st.header("About")
+    st.markdown("""
+    #### About Dataset
+    This dataset is recreated using offline augmentation from the original dataset. The original dataset can be found on this github repo.
+    This dataset consists of about 87K RGB images of healthy and diseased crop leaves which is categorized into 38 different classes. The total dataset is divided into 80/20 ratio of training and validation set preserving the directory structure.
+    A new directory containing 33 test images is created later for prediction purposes.
+
+    #### Content
+    1. Train (70,295 images)
+    2. Test (33 images)
+    3. Validation (17,572 images)
+    """)
+
